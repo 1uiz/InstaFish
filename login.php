fc529f4a6b6b3b77cb531f9deea32d86fd60c010
Diaz4779 <jdiazflores@csumb.edu>
2015-04-29 22:10:33 +0000

    Added log table for succesful/unsuccesful logins

--- login.php
+++ login.php
@@ -1,30 +1,52 @@
 <?php
-session_start();
-require 'test_dbConnection.php';
 
-$dbConn = getConnection();
+    session_start();
+    include 'test_dbConnection.php';
 
-$username = $_POST['username'];
-$password = sha1($_POST['password']);
+    $dbConn = getConnection();
 
-$sql = "SELECT * FROM instaUsers WHERE username = :username AND password = :password";
-$namedParameters = array();
-$namedParameters[':username'] = $username;
-$namedParameters[':password'] = $password;
-$stmt = $dbConn -> prepare($sql);
-$stmt->execute($namedParameters);
-$result = $stmt->fetch(); //We are expecting one record
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-if (empty($result)) {
+    // Prevent sql injection
+    $sql = "SELECT * from `instaUsers` WHERE username = :username AND password = :password";
+    $namedParameters = array();
+    $namedParameters[':username'] = $username;
+    $namedParameters[':password'] = hash("sha1", $password);
 
-    header("Location: login.php?error=WRONG USERNAME OR PASSWORD");
-   
-} else {
-
-    $_SESSION['username']  = $result['username'];
-    $_SESSION['adminName'] = $result['firstName'] . " " . $result['lastName'];
-    header("Location: fish_test.html");
+    $stmt = $dbConn -> prepare($sql);
+    $stmt -> execute($namedParameters); //We are expecting one record
+    $result = $stmt -> fetch();
     
-}
+
+    if(empty($result)){
+        $sql = "INSERT INTO log (userName, userId, isSuccesfullLogin) VALUES (:uName, :uId, :isSuccessful)";
+        $namedParameters = array();
+        $namedParameters[":uName"] = $result['username'];
+        $namedParameters[":uId"] = $result['userID'];
+        $namedParameters[":isSuccessful"] = 0;
+        
+        $stmt = $dbConn -> prepare($sql);
+        $stmt -> execute($namedParameters);
+        header("Location: login.html?error=WRONG USERNAME OR PASSWORD");
+        
+    }
+    else{
+        // insert log record to database
+        $_SESSION['username'] = $result['username'];
+        $_SESSION['adminName'] = $result['firstName'] . " " . $result['lastName'];
+        
+
+        $sql = "INSERT INTO log (userName, userId, isSuccesfullLogin) VALUES (:uName, :uId, :isSuccessful)";
+        $namedParameters = array();
+        $namedParameters[":uName"] = $result['username'];
+        $namedParameters[":uId"] = $result['userID'];
+        $namedParameters[":isSuccessful"] = 1;
+        
+        $stmt = $dbConn -> prepare($sql);
+        $stmt -> execute($namedParameters);
+
+        header("Location: fish_test.html");
+    }
 
 ?>
