a08165fd6b715b77e1a3b79e062e8c1e5fe9891a
Diaz4779 <jdiazflores@csumb.edu>
2015-05-05 00:44:05 +0000

    Updated db file

--- dbConnection.php
+++ dbConnection.php
@@ -1,9 +1,9 @@
 <?php
 	function getConnection(){
 		$host = "localhost";
-		$dbname = "test";
-		$username = "dbUser";
-		$password = "test";
+		$dbname = "Instafish";
+		$username = "root";
+		$password = "s3cr3t";
 		$dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     	$dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		return $dbConn;
